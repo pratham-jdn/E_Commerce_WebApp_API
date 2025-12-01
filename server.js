533d12c1c536977f2a13951607f6f2b859dbeca6
pratham-jdn <2003prathamkumar@gmail.com>
2025-12-01 14:28:56 +0530

    cors issue

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "https://e-commerce-web-app-frontend-six.vercel.app/",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "Auth"],
   credentials: true
